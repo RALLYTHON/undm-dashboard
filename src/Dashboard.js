53ba52509427aa3317a4f791c24239ff9922792c
RALLYTHON <bwhitley1212@gmail.com>
2025-03-28 20:18:27 -0400

    Update Dashboard.js

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -29,7 +29,7 @@ class Dashboard extends React.Component{
 
     poll = () => {
         this.setState({pollingCount: this.state.pollingCount + 1, oldDonations: this.state.donations});
-        fetch('https://events.dancemarathon.com/api/events/4589/donations?limit=5')
+        fetch('https://events.dancemarathon.com/api/events/6240/donations?limit=5')
             .then(response => response.json())
             .then(data => {
                 this.setState({donations: data})
@@ -61,10 +61,10 @@ class Dashboard extends React.Component{
         </Row>
         <Row style={{display: "inline-block"}}>
             <div className="countdown">
-                <span>🚀</span>
-                <Countdown date={new Date("Feb 26, 2022 23:45:00")} daysInHours={true}/>
-                <span style={{padding: 0}}> until Huskerthon blastoff!</span>
-                <span>🚀</span>
+                <span>🧁</span>
+                <Countdown date={new Date("March 29, 2025 23:59:59")} daysInHours={true}/>
+                <span style={{padding: 0}}> until RALLYTHON SUGAR LAUNCH!</span>
+                <span>🧁</span>
             </div>
         </Row>
         {/* <Row>
